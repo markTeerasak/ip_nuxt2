21ec7218884dfa130276e238cad61139db7381d0
markTeerasak <twichai4869@gmail.com>
2023-03-05 21:39:04 +0700

    add slid in home page

--- layouts/user.vue
+++ layouts/user.vue
@@ -1,15 +1,30 @@
 <template>
     <v-app id="inspire">
+        <v-overlay v-model="overlay" contained class="align-center justify-center">
+            <v-row class="my-3">
+                <v-btn color="success" @click="overlay = false">
+                    <v-icon class="me-1">mdi-logout-variant</v-icon>
+                    logout
+                </v-btn>
+            </v-row>
+            <v-row class="my-3">
+                <v-btn color="error" @click="overlay = false">
+                    <v-icon class="me-1">mdi-logout-variant</v-icon>
+                    logout
+                </v-btn>
+            </v-row>
+        </v-overlay>
         <v-app-bar app=flat>
             <v-avatar :color="$vuetify.breakpoint.smAndDown ? 'grey darken-1' : 'transparent'" size="32"></v-avatar>
 
             <v-tabs centered class="ml-n9" color="grey darken-1">
-                <v-tab v-for="link in links" :key="link">
-                    {{ link }}
+                <v-tab v-for="(item, link) in links" :key="link" :to="item.to">
+                    {{ item.title }}
                 </v-tab>
             </v-tabs>
-
-            <v-avatar class="hidden-sm-and-down" color="grey darken-1 shrink" size="32"></v-avatar>
+            <v-btn @click="overlay = !overlay" icon="mdi-account-circle">
+                <v-avatar class="hidden-sm-and-down" color="grey darken-1 shrink" size="32" ></v-avatar>
+            </v-btn>
         </v-app-bar>
         <v-main>
             <v-container>
@@ -21,14 +36,16 @@
                     </v-col>
 
                     <v-col cols="12" sm="8">
-                        <v-sheet min-height="70vh" rounded="lg">
+                        <v-sheet min-height="70vh" rounded="lg" class="px-5 py-7">
                             <!--  -->
+                            <nuxt />
                         </v-sheet>
                     </v-col>
 
                     <v-col cols="12" sm="2">
-                        <v-sheet rounded="lg" min-height="268">
+                        <v-sheet rounded="lg" min-height="268" class="py-3 px-4">
                             <!--  -->
+                            <Info/>
                         </v-sheet>
                     </v-col>
                 </v-row>
@@ -38,14 +55,27 @@
 </template>
   
 <script>
+import Info from '../components/info.vue'
 export default {
     data: () => ({
         links: [
-            'Dashboard',
-            'Messages',
-            'Profile',
-            'Updates',
+        {
+          title: 'home',
+          to: '/',
+        },
+        {
+          title: 'info',
+          to: '/info',
+        },
+        {
+          title: 'schedule',
+          to: '/schedule',
+        }
         ],
+        overlay: false,
     }),
+    components: {
+        Info
+    }
 }
 </script>
