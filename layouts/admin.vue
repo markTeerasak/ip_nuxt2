f66386e16f7aac38b39a8f25ac66f8d4d04f5b97
markTeerasak <twichai4869@gmail.com>
2023-03-05 20:15:41 +0700

    add student page

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -1,7 +1,7 @@
 <template>
     <v-app>
         <v-layout row wrap>
-            <v-navigation-drawer permanent expand-on-hover>
+            <v-navigation-drawer permanent expand-on-hover class="px-1">
                 <v-list>
                     <v-list-item class="px-2">
                         <v-list-item-avatar>
