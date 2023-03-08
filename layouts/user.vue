31fd753eb5212da9cb7927f7541e69946d65482c
Mr.Kitsune <84368356+Kitsune-cop@users.noreply.github.com>
2023-03-08 14:17:04 +0700

    Update user.vue

--- layouts/user.vue
+++ layouts/user.vue
@@ -1,5 +1,7 @@
 <template>
     <v-app id="inspire">
+        <v-app-bar flat>
+            <v-avatar :color="$vuetify.breakpoint.smAndDown ? 'grey darken-1' : 'transparent'" size="32"></v-avatar>
     <v-overlay v-model="overlay" contained class="align-center justify-center">
             <v-row class="my-3">
                 <v-btn color="success" @click="overlay = false">
@@ -16,15 +18,43 @@
         </v-overlay>
         <v-app-bar flat>
             <v-avatar :color="$vuetify.breakpoint.smAndDown ? 'grey darken-1' : 'transparent'" size="32"></v-avatar>
-            
             <v-tabs centered class="ml-n9" color="grey darken-1">
                 <v-tab v-for="(item, link) in links" :key="link" :to="item.to">
                     {{ item.title }}
                 </v-tab>
             </v-tabs>
-            <v-btn @click="overlay = !overlay" icon mdi-account-circle>
-                <v-avatar class="hidden-sm-and-down" color="grey darken-1 shrink" size="32" ></v-avatar>
-            </v-btn>
+
+            <v-menu bottom min-width="200px" rounded offset-y>
+                <template v-slot:activator="{ on }">
+                    <v-btn icon x-large v-on="on">
+                        <v-avatar color="brown" size="48">
+                            <span class="white--text text-h5">{{ user.initials }}</span>
+                        </v-avatar>
+                    </v-btn>
+                </template>
+                <v-card>
+                    <v-list-item-content class="justify-center">
+                        <div class="mx-auto text-center">
+                            <v-avatar color="brown">
+                                <span class="white--text text-h5">{{ user.initials }}</span>
+                            </v-avatar>
+                            <h3>{{ user.fullName }}</h3>
+                            <p class="text-caption mt-1">
+                                {{ user.email }}
+                            </p>
+                            <v-divider class="my-3"></v-divider>
+                            <v-btn @click="hendleLogout()">
+                                <v-icon>mdi-logout</v-icon>
+                                Logout
+                            </v-btn>
+                        </div>
+                    </v-list-item-content>
+                </v-card>
+            </v-menu>
+            <div class="mx-3 hidden-sm-and-down"></div>
+            <!-- <v-btn icon mdi-account-circle>
+                <v-avatar class="hidden-sm-and-down" color="grey darken-1 shrink" size="32"></v-avatar>
+            </v-btn> -->
         </v-app-bar>
         <v-main>
             <v-container>
@@ -46,6 +76,8 @@
                     <v-col cols="12" sm="2">
                         <v-card rounded="lg" min-height="268" class="py-3 px-4">
                             <!--  -->
+                            <Info />
+                        </v-sheet>
                             <Info/>
                         </v-card>
                     </v-col>
@@ -61,6 +93,18 @@ import work from '../components/work.vue'
 export default {
     data: () => ({
         links: [
+            {
+                title: 'home',
+                to: '/',
+            },
+            {
+                title: 'info',
+                to: '/info',
+            },
+            {
+                title: 'schedule',
+                to: '/schedule',
+            }
         {
           title: 'home',
           to: '/',
@@ -75,9 +119,29 @@ export default {
         },
 
         ],
-        overlay: false,
+        user: {
+            initials: 'Jt',
+            fullName: 'John Doe',
+            email: 'john.doe@doe.com',
+        },
     }),
     components: {
+        Info
+    },
+    methods: {
+        hendleLogout() {
+            sessionStorage.clear()
+            window.location = './login'
+            
+        },
+        checkLogin() {
+            if(!sessionStorage.getItem('user_id')){
+                window.location = './login'
+            } 
+        }
+    },
+    mounted() {
+        this.checkLogin();
         Info,
         work
     }
